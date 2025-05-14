994561673699d5eab8780c47cb5c9293eafeb1aa
AdamT3H <adamogorodnik@gmail.com>
2025-05-14 21:46:12 +0300

    fix: callback

--- app/api/success-redirect/route.ts
+++ app/api/success-redirect/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function POST() {
   return NextResponse.redirect('https://one-two-smile.vercel.app/shop/success', 302);
