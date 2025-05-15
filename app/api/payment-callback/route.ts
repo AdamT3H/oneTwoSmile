d39923dddcab2f35eeeef8353cab1b0522896377
AdamT3H <adamogorodnik@gmail.com>
2025-05-15 16:01:29 +0300

    fix: fixwarehouseName

--- app/api/payment-callback/route.ts
+++ app/api/payment-callback/route.ts
@@ -1,29 +1,42 @@
-import { NextRequest } from 'next/server';
+import { NextRequest } from "next/server";
+import { supabase } from '@/lib/supabase';
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
-    const { transactionStatus } = body;
-    console.log("HHH!!!", body)
+    const { transactionStatus, orderReference } = body;
+    console.log("HHH!!!", body);
 
-    if (transactionStatus === 'Approved') {
-      console.log('✅ УСПІШНА ОПЛАТА — НАДСИЛАЮ EMAIL!');
-      return new Response(JSON.stringify({ reason: 'Success' }), {
+    if (transactionStatus === "Approved") {
+      console.log("✅ УСПІШНА ОПЛАТА — НАДСИЛАЮ EMAIL!", orderReference);
+
+    //   const { data: order, error } = await supabase
+    //     .from("orders")
+    //     .select("*")
+    //     .eq("order_reference", orderReference)
+    //     .single();
+
+    //   if (error || !order) {
+    //     console.error("❌ Не вдалося знайти замовлення в базі:", error);
+    //     return new Response("Order not found", { status: 404 });
+    //   }
+
+      return new Response(JSON.stringify({ reason: "Success" }), {
         status: 200,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
       });
     }
 
-    return new Response('Ignored', { status: 200 });
+    return new Response("Ignored", { status: 200 });
   } catch (error) {
-    console.error('❌ Помилка в callback:', error);
-    return new Response('Server Error', { status: 500 });
+    console.error("❌ Помилка в callback:", error);
+    return new Response("Server Error", { status: 500 });
   }
 }
 
 export async function GET() {
-  return new Response('✅ Callback route is alive (GET)', { status: 200 });
+  return new Response("✅ Callback route is alive (GET)", { status: 200 });
 }
 
 // import { NextRequest } from 'next/server';
