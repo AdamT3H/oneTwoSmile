715ac2f071b0e8a97c7dd7f38dc38ea68a691ad5
AdamT3H <adamogorodnik@gmail.com>
2025-05-17 13:02:12 +0300

    url ref fix

--- app/api/wayforpay-return/route.ts
+++ app/api/wayforpay-return/route.ts
@@ -1,3 +1,13 @@
-export async function POST() {
-    return Response.redirect('https://one-two-smile.vercel.app/shop/success', 303);
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    const ref = body.orderReference;
+  
+    if (!ref) {
+      return new Response('Missing order reference', { status: 400 });
+    }
+  
+    return Response.redirect(`https://one-two-smile.vercel.app/shop/success?ref=${ref}`, 303);
   }
+  
