ad3b768dd1840d586904790c611e0260ced186d9
AdamT3H <adamogorodnik@gmail.com>
2025-05-18 00:16:09 +0300

    fix: succsess page no card2

--- app/shop/successnc/page.tsx
+++ app/shop/successnc/page.tsx
@@ -1,11 +1,24 @@
 import ShopNav from "@/app/components/shopNav/ShopNav";
 import React from "react";
+import styles from "./page.module.css";
 
 export default function SuccessPage() {
   return (
     <div className="w-full">
       <ShopNav />
-      <>ХАХАХХАХАХАХА УГАР НУ ТИ І ЛО</>
+      <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.mainText}>Ви створили замовлення!</h1>
+          <>
+            <p className={styles.thankText}>
+              Дякуємо! Ваше замовлення прийнято.
+            </p>
+          </>
+        <p className={styles.alertText}>
+          Очікуйте лист на електронну пошту з підтвердженням.
+        </p>
+      </div>
+    </div>
     </div>
   );
 }
