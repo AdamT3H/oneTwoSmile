55ee073425e5950abcd3812a28bc89292f60185c
AdamT3H <adamogorodnik@gmail.com>
2025-05-17 12:44:29 +0300

    url ref

--- app/api/wayforpay-return/route.ts
+++ app/api/wayforpay-return/route.ts
@@ -1,3 +1,12 @@
-export async function POST() {
-    return Response.redirect('https://one-two-smile.vercel.app/shop/success', 303);
+import { NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const ref = searchParams.get('ref');
+
+  if (!ref) {
+    return new Response('Missing order reference', { status: 400 });
   }
+
+  return Response.redirect(`https://one-two-smile.vercel.app/shop/success?ref=${ref}`, 303);
+}
